45f8e455da6c214771738fa704071f49c871a008
MedFadl <mohamedmedfadl@gmail.com>
2026-05-17 02:24:44 +0300

    first commit

---
+++ src/main/java/com/medhat/sore/SoreApplication.java
@@ -0,0 +1,13 @@
+package com.medhat.sore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medhat/sore/SoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhat.sore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
