d487673f743b5289af85b4ed2cab4764be38fc89
MedFadl <mohamedmedfadl@gmail.com>
2026-05-17 22:56:46 +0300

    added new dependency + application properties value

--- src/main/java/com/medhat/sore/HomeController.java
+++ src/main/java/com/medhat/sore/HomeController.java
@@ -1,13 +1,22 @@
 package com.medhat.sore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
     @RequestMapping("/")
     public String index()
     {
+        return getViewName();
+    }
+
+    private String getViewName()
+    {
+        System.out.println("appName: " + appName);
         return "index.html";
     }
 }
